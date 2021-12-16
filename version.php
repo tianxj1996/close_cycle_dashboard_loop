c40fa8ab45c39b507a6396ddf51720b6bea59ed4
renehilgemann <rene.hilgemann@stud.uni-due.de>
2021-12-16 18:52:50 +0100

    First commit with version template

---
+++ version.php
@@ -0,0 +1,32 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Block version
+ *
+ * @package    block_closed_loop_support
+ * @copyright  2022 Rene Hilgemann
+ * @author     Rene Hilgemann <rene.hilgemann@gmx.net>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->component = 'block_closed_loop_support';
+$plugin->version   = 2021121601; //TODO: Modify at the end
+$plugin->requires  = 2021051704;
+$plugin->maturity = MATURITY_ALPHA;
+$plugin->release = 'v3.11-r1'; //Release for Moodle 3.11 Stable
